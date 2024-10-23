f35272f201349a3585c9aa63c02662c2e3ea1f97
Kim Arsinenko <124343207+Arsinenko@users.noreply.github.com>
2024-10-23 15:35:59 +0300

    c# linked list

---
+++ linkedList/c#/LinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void PrintList(LinkedList<int> list)
+    {
+        foreach (var VARIABLE in list)
+        {
+            Console.Write($"{VARIABLE} -> ");
+        }
+        Console.Write("NULL");
+    }  
+    public static void Main(string[] args)
+    {
+        var numbers = new LinkedList<int>();
+        //add element 
+        numbers.AddLast(10);
+        numbers.AddLast(15);
+        numbers.AddLast(25);
+        numbers.AddFirst(1);
+        
+        //remove elements
+        numbers.RemoveFirst();
+        numbers.RemoveLast();
+        
+        
+        
+        PrintList(numbers);
+        
+        
+        
+        
+    }
+}
